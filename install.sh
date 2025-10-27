9658cdb19162c35d8a63d27c7b688ad7289068c4
Minh Vu <minh.vuduc@skybull.studio>
2025-10-27 15:27:07 +0700

    Remove unnecessary chown command from install script.

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ docker compose down -v
 docker compose build --no-cache
 docker compose up -d
 
-chown -R www-data:www-data docker/laravel/
 docker compose exec -T laravel_app php artisan migrate
 
 rm ./docker/laravel/public/build/ -r
