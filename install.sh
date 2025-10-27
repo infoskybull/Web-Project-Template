78cd58c71c36eb6e5f9221e6c20c3a6d67901df9
Vasilii Kozlov <vasilii@skybull.studio>
2025-10-27 04:46:09 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ docker compose down -v
 docker compose build --no-cache
 docker compose up -d
 
+sleep 2
+
 chown -R www-data:www-data docker/laravel/
 docker compose exec -T laravel_app php artisan migrate
